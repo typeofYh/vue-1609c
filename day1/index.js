5a2cb595cb7640a9bff0c9ce601809d009b57f59
yihang <1341674619@qq.com>
2018-12-25 11:07:06 +0800

    day1

---
+++ day1/index.js
@@ -0,0 +1,76 @@
+
+//     var str = function(){
+//         console.log('hello')
+//     }
+    
+//     function str(){
+//         console.log('hi');
+//     }
+    
+//     var str = 'bawei';
+    
+//     console.log(str);
+
+
+// console.log(global.str);  commonjs规范
+// console.log(window.str); 
+
+// console.log(obj);
+// let obj = {
+//     classname:"1609C"
+// };
+
+// if(10>5){
+//     let str = 'bawei';
+//     console.log(str);
+//     for(let i=0;i<10;i++){
+//         let str = '1609C';
+//         let i = 20;
+//         console.log(str);
+//         console.log(i);
+//     }
+//     let i = 30;
+//     console.log(i);
+// }
+// console.log(str);
+
+
+// for(var i=0;i<10;i++){
+//     // let a 
+//     (function(n){
+//         setTimeout(function(){
+//             console.log(n);
+//         },0)
+//     })(i)
+//     // setTimeout(function(){
+//     //     console.log(i);  // 0-9
+//     // },0)
+// }
+
+// console.log(a);
+// const a;
+// const a = {
+//     name:'1609C'
+// };
+// a.person = 29;
+// a = {};
+// console.log(a);
+
+if(10>5){
+    // console.log(str);
+    const str = 'bawei';
+    {
+        const str = '1609C';
+        console.log(str);
+    }
+}
+
+// console.log(str);
+
+
+
+
+
+
+
+
